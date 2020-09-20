5e2fcf7f0a7e696888c8a6bdf733d38e0d50df08
zero <sagnikdas98@hotmail.com>
2020-09-20 10:27:29 -0400

    structured

--- scripts/python_scripts/pdf_to_image.py
+++ scripts/python_scripts/pdf_to_image.py
@@ -7,4 +7,4 @@ pdf_config = json.load(open(pdf_config_file, 'r'))
 
 
 
-images = pdf2image.convert_from_path(pdf_config['pdf_path'],output_folder=pdf_config['output_image_path'], fmt="jpeg", )
+images = pdf2image.convert_from_path(pdf_config['src_path'],output_folder=pdf_config['dest_path'], fmt="jpeg", )
